34775fb686a13e6884afada8b6ddb149530cd956
chenbiao <149027310@qq.com>
2024-04-30 16:05:31 +0800

    query

--- tpl/dal_ext_tpl.go
+++ tpl/dal_ext_tpl.go
@@ -29,19 +29,8 @@ func (d *{{.LowerCamelName}}ExtDao) MustGetById(ctx *dgctx.DgContext, tc *daog.T
 	return {{.LowerCamelName}}, nil
 }
 
-func (d *{{.LowerCamelName}}ExtDao) Query(ctx *dgctx.DgContext, tc *daog.TransContext, req *model.Query{{.GoTable}}Req) (*page.PageList[{{.GoTable}}], error) {
-	matcher := daog.NewMatcher()
-	{{range $index, $column := .QueryColumns -}}
-	{{- if eq $column.ModelType "string"}}
-	if req.{{$column.GoName}} != "" {
-		matcher.Eq({{$.GoTable}}Fields.{{$column.GoName}}, req.{{$column.GoName}})
-	}
-	{{- else if contains $column.ModelType "int"}}
-	if req.{{$column.GoName}} != 0 {
-		matcher.Eq({{$.GoTable}}Fields.{{$column.GoName}}, req.{{$column.GoName}})
-	}
-	{{- end }}
-	{{- end }}
+func (d *{{.LowerCamelName}}ExtDao) Page(ctx *dgctx.DgContext, tc *daog.TransContext, req *model.Query{{.GoTable}}Req) (*page.PageList[{{.GoTable}}], error) {
+	matcher := d.buildMatcher(req)
 	
 	count, err := {{.GoTable}}Dao.Count(tc, matcher)
 	if err != nil {
@@ -54,7 +43,7 @@ func (d *{{.LowerCamelName}}ExtDao) Query(ctx *dgctx.DgContext, tc *daog.TransCo
 	
 	{{.LowerCamelName}}List, err := {{.GoTable}}Dao.QueryPageListMatcher(tc, matcher, daog.NewPager(req.PageSize, req.PageNo), daog.NewDescOrder({{$.GoTable}}Fields.CreatedAt))
 	if err != nil {
-		dglogger.Errorf(ctx, "{{.GoTable}}Dao.QueryListMatcher error: %v", err)
+		dglogger.Errorf(ctx, "{{.GoTable}}Dao.QueryPageListMatcher error: %v", err)
 		return nil, dgerr.SYSTEM_ERROR
 	}
 	if len({{.LowerCamelName}}List) == 0 {
@@ -62,6 +51,35 @@ func (d *{{.LowerCamelName}}ExtDao) Query(ctx *dgctx.DgContext, tc *daog.TransCo
 	}
 	
 	return page.ListOf(req.PageNo, req.PageSize, int(count), {{.LowerCamelName}}List), nil
+}
+
+func (d *{{.LowerCamelName}}ExtDao) List(ctx *dgctx.DgContext, tc *daog.TransContext, req *model.Query{{.GoTable}}Req) ([]*{{.GoTable}}, error) {
+	matcher := d.buildMatcher(req)
+
+	{{.LowerCamelName}}List, err := {{.GoTable}}Dao.QueryListMatcher(tc, matcher, daog.NewDescOrder({{$.GoTable}}Fields.CreatedAt))
+	if err != nil {
+		dglogger.Errorf(ctx, "{{.GoTable}}Dao.QueryListMatcher error: %v", err)
+		return nil, dgerr.SYSTEM_ERROR
+	}
+	
+	return {{.LowerCamelName}}List, nil
+}
+
+func (d *{{.LowerCamelName}}ExtDao) buildMatcher(req *model.Query{{.GoTable}}Req) daog.Matcher {
+	matcher := daog.NewMatcher()
+	{{range $index, $column := .QueryColumns -}}
+	{{- if eq $column.ModelType "string"}}
+	if req.{{$column.GoName}} != "" {
+		matcher.Eq({{$.GoTable}}Fields.{{$column.GoName}}, req.{{$column.GoName}})
+	}
+	{{- else if contains $column.ModelType "int"}}
+	if req.{{$column.GoName}} != 0 {
+		matcher.Eq({{$.GoTable}}Fields.{{$column.GoName}}, req.{{$column.GoName}})
+	}
+	{{- end }}
+	{{- end }}
+
+	return matcher
 }
 
 `
