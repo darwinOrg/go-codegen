d72468d5f4c944de7da85958cc724ff875a91492
chenbiao <chenbiao@globalpand.cn>
2024-05-01 17:41:58 +0800

    enum

--- tpl/enum_tpl.go
+++ tpl/enum_tpl.go
@@ -2,12 +2,14 @@ package tpl
 
 var EnumTpl = `package enum
 
-{{range $column, $enums := .EnumMap}}
-type {{$.GoTable}}{{$column.GoName}} {{$column.DbType}}
+{{range $column, $enums := .EnumMap}}type {{$.GoTable}}{{$column.GoName}} {{$column.DbType}}
 var (
-	{{range $index, $enum := $enums}}
-	{{$.GoTable}}{{$column.GoName}}_{{$enum.Key}} {{$.GoTable}}{{$column.GoName}} = {{if eq $column.DbType "string"}}"{{$enum.Key}}"{{else}}{{$enum.Key}}{{end}} // {{$enum.Value}}
+	{{range $index, $enum := $enums}}{{$.GoTable}}{{$column.GoName}}_{{$enum.Key}} {{$.GoTable}}{{$column.GoName}} = {{if eq $column.DbType "string"}}"{{$enum.Key}}"{{else}}{{$enum.Key}}{{end}} // {{$enum.Value}}
 	{{end}}
+	{{$.GoTable}}{{$column.GoName}}Map = map[{{$column.DbType}}]string{
+		{{range $index, $enum := $enums}}{{$column.DbType}}({{$.GoTable}}{{$column.GoName}}_{{$enum.Key}}): "{{$enum.Value}}",
+		{{end}}
+	}
 )
 {{end}}
 `
