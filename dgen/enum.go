9164d1d9196116403256af7a160875a8bcfabfe2
chenbiao <149027310@qq.com>
2025-04-12 20:56:25 +0800

    up

---
+++ dgen/enum.go
@@ -0,0 +1,22 @@
+package dgen
+
+const (
+	InterfaceTypePage   = "分页"
+	InterfaceTypeList   = "列表"
+	InterfaceTypeCreate = "新建"
+	InterfaceTypeModify = "修改"
+	InterfaceTypeDelete = "删除"
+	InterfaceTypeDetail = "详情"
+)
+
+const (
+	MethodTypePost = "Post"
+	MethodTypeGet  = "Get"
+)
+
+const (
+	LogLevelAll    = "全部"
+	LogLevelParam  = "请求参数"
+	LogLevelReturn = "返回响应"
+	LogLevelNone   = "无"
+)
