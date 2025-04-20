9fc989fb193e3272a7b48abfc035459c4636542f
chenbiao <chenbiao@globalpand.cn>
2025-04-20 15:53:07 +0800

    up

--- dgen/swagger.go
+++ dgen/swagger.go
@@ -2,6 +2,7 @@ package dgen
 
 import (
 	dgcoll "github.com/darwinOrg/go-common/collection"
+	"github.com/darwinOrg/go-common/result"
 	"github.com/darwinOrg/go-common/utils"
 	dgcfg "github.com/darwinOrg/go-config"
 	"github.com/darwinOrg/go-swagger"
@@ -9,6 +10,14 @@ import (
 	"github.com/go-openapi/spec"
 	"os"
 )
+
+type pageList struct {
+	PageNo     int   `json:"pageNo" binding:"required" remark:"页码"`
+	PageSize   int   `json:"pageSize" binding:"required" remark:"每页记录数"`
+	TotalCount int   `json:"totalCount" remark:"总记录数"`
+	TotalPages int   `json:"totalPages" remark:"总页数"`
+	List       []any `json:"list" remark:"列表记录"`
+}
 
 func SyncToApifoxDefault(entireModel *EntireModel) {
 	dgcfg.LoadDotEnv(dgcfg.MustConfRoot())
@@ -40,14 +49,23 @@ func BuildSwaggerProps(entireModel *EntireModel) spec.SwaggerProps {
 
 	req.RequestApis = dgcoll.FlatMapToList(entireModel.Interfaces, func(imd *InterfaceModelData) []*wrapper.RequestApi {
 		return dgcoll.MapToList(imd.Models, func(im *InterfaceModel) *wrapper.RequestApi {
-			return &wrapper.RequestApi{
-				Method:         im.MethodType,
-				BasePath:       imd.RoutePrefix,
-				RelativePath:   im.RelativePath,
-				Remark:         im.Remark,
-				RequestObject:  im.RequestModelObject,
-				ResponseObject: im.ResponseApiObject,
+			ra := &wrapper.RequestApi{
+				Method:        im.MethodType,
+				BasePath:      imd.RoutePrefix,
+				RelativePath:  im.RelativePath,
+				Remark:        im.Remark,
+				RequestObject: im.RequestModelObject,
 			}
+
+			if im.ResponseApiObject != nil {
+				ra.ResponseObject = im.ResponseApiObject
+			} else if im.ResponseApiObject == nil && im.ResponseModelObject != nil {
+				ra.ResponseObject = result.Success(&pageList{
+					List: []any{im.ResponseModelObject},
+				})
+			}
+
+			return ra
 		})
 	})
 
